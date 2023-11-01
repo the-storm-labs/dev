907ffd5fa7297f9a41f85fb6a00549d90fb52b5a
Renan <kruger.renan.eng@gmail.com>
2023-11-01 10:30:54 -0300

    enhanced

--- packages/contracts/contracts/Dependencies/TellorCaller.sol
+++ packages/contracts/contracts/Dependencies/TellorCaller.sol
@@ -16,22 +16,24 @@ import "usingtellor/contracts/UsingTellor.sol";
 */
 contract TellorCaller is UsingTellor {
        
+
+    mapping (uint256 => bytes32 ) public queryId;
+
     constructor (address payable _tellorMasterAddress) UsingTellor(_tellorMasterAddress) {
-        
-    }
-
-     function getTellorCurrentValue(uint256 _requestId) external view returns ( bool, uint256, uint256)    {      
-      require( _requestId ==1, "TellorCaller: pair not registered");
-
       bytes memory _queryData = abi.encode("SpotPrice", abi.encode("eth", "usd"));
       bytes32 _queryId = keccak256(_queryData);
-      
-      (bytes memory _value, uint256 _timestampRetrieved) = getDataBefore(_queryId, block.timestamp - 20 minutes);
+      queryId[1] =_queryId;
+
+    }
+
+     function getTellorCurrentValue(uint256 _requestId) external view returns ( bool, uint256, uint256)    {
+
+      (bytes memory _value, uint256 _timestampRetrieved) = getDataBefore(queryId[_requestId], block.timestamp - 20 minutes);
       if (_timestampRetrieved == 0) return (false, 0, _timestampRetrieved);
       require(block.timestamp - _timestampRetrieved < 3 hours);
       uint256 value = abi.decode(_value, (uint256));
       if (value > 0) return (true, value, _timestampRetrieved); 
-      return (false, 0 , _timestampRetrieved) ;
+      return (false, 0 , _timestampRetrieved);
     
     }
 }
